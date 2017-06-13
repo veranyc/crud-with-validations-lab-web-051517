a713dc62ebe83d0670c99db07fe5080a15024a85
veranyc <veranyc@gmail.com>
2017-06-13 17:40:05 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :released, inclusion: {in: [true,false]}
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |released|
+    released.validates :release_year, presence: true
+    released.validates :release_year, numericality: {less_than_or_equal_to: Date.current.year}
+  end
+
+  def released?
+    released
+  end
+
+end
